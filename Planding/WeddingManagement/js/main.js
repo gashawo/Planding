9c86799c607393c25ac9e7e877e2be070bf06c57
gashawo moges <gashawo89@gmail.com>
2019-05-28 23:05:18 +0300

    PlanDing

---
+++ Planding/WeddingManagement/js/main.js
@@ -0,0 +1,21 @@
+function checkAuth(){
+    firebase.auth().onAuthStateChanged(function (user) {
+        console.log(user);
+        if (user) {
+            alert("Signed In");
+        } else {
+            alert("Not Signed In");
+        }
+    });
+}
+
+function redirectToDashboard(){
+    window.location.href="dashboard.html";
+}
+
+$("#logout").click(function(e){
+    e.preventDefault();
+    firebase.auth().signOut().then(function(){
+        alert("Logged Out");
+    });
+});
